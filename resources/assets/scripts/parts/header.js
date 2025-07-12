9a229b421775196b1a2933c5987c639d38dd7708
krupa <krupa@karmadhi.com>
2025-07-12 17:12:22 +0530

    header-remaining

--- resources/assets/scripts/parts/header.js
+++ resources/assets/scripts/parts/header.js
@@ -1,18 +1,49 @@
 export class Header {
     init() {
-        this.MegaMenu();
+        this.HeaderHover();
+        this.MenuToggle();
     }
 
-    MegaMenu() {
-        $(".mega-link").hover(
-            function () {
-                $(this).addClass("mega-active");
-                $("header").addClass("header-active");
-            },
-            function () {
-                $(this).removeClass("mega-active");
-                $("header").removeClass("header-active");
+    HeaderHover() {
+        $(document).ready(function () {
+            function handleWindowResizeDeskSize() {
+                var windowWidth = $(window).width();
+                if (windowWidth >= 1024) {
+                    $(".mega-link").hover(
+                        function () {
+                            $(this).addClass("mega-active");
+                            $("header").addClass("header-active");
+                        },
+                        function () {
+                            $(this).removeClass("mega-active");
+                            $("header").removeClass("header-active");
+                        }
+                    )
+                }
             }
-        )
+
+            handleWindowResizeDeskSize();
+            $(window).resize(handleWindowResizeDeskSize);
+        });
+    }
+     MenuToggle() {
+        $(".menu-toggle").click(function (e) {
+            e.preventDefault();
+
+            if (!$(".menu-toggle").hasClass("activate")) {
+                // Open menu
+                $(".menu-toggle").addClass("activate");
+                $(".navigation").removeClass("d-none");
+                $(".header").addClass("res-header-active");
+                $("html").addClass("overflow-hidden");
+            } else {
+                // Close menu
+                $(".menu-toggle").removeClass("activate");
+                $(".navigation").addClass("d-none");
+                $(".header").removeClass("res-header-active");
+                $("html").removeClass("overflow-hidden");
+            }
+            $(".mega-link").removeClass("res-menu-active");
+        });
     }
 }
--- templates/header.php
+++ templates/header.php
@@ -12,7 +12,7 @@ $schedule = get_field('schedule', 'option');
     <div class="header-top bg-white py-2">
         <div class="container">
             <div class="row justify-content-between">
-                <div class="col-4 d-flex">
+                <div class="col-xl-4 col-lg-4 d-flex">
                     <div class="clock-img d-flex justify-content-center align-items-center me-2">
                         <img class="" src="<?php echo get_template_directory_uri(); ?>/templates/icon/blue-clock.svg"
                             alt="">
@@ -26,37 +26,35 @@ $schedule = get_field('schedule', 'option');
                             if ($schedule_open_time != 'Closed') {
                                 $schedule_open_status = 'Open';
                                 $schedule_entry = 'd-block';
-
                             } else {
                                 $schedule_open_status = '';
                                 $schedule_entry = 'd-none';
-
                             }
                             if ($currnt_day == $schedule_open_day) {
                                 $cotent_visinle = 'd-flex';
                             } else {
                                 $cotent_visinle = 'd-none';
                             }
-                            ?>
+                        ?>
 
                             <div class=" <?php echo $cotent_visinle; ?>">
-                                <div class="bg-1B2995-label sans font12 leading20 px-2 text-1B2995 radius5 me-2">
+                                <div class="bg-1B2995-label sans font12 leading20 res-font10 px-2 text-1B2995 radius5 me-2">
 
-                                    <?php echo $schedule_open_status; ?>         <?php echo $schedule_open_time; ?>
+                                    <?php echo $schedule_open_status; ?> <?php echo $schedule_open_time; ?>
                                 </div>
-                                <div class="sans-italic font12 leading20 text-1B2995 <?php echo $schedule_entry; ?>">
+                                <div class="sans-italic font12 leading20 res-font10 res-leading20 text-1B2995 <?php echo $schedule_entry; ?>">
                                     (Last entry at <?php echo $schedule_open_last_entry; ?>*)
                                 </div>
                             </div>
                         <?php endforeach; ?>
                     <?php endif; ?>
                 </div>
-                <div class="col-5">
-                    <ul class="list-none d-flex justify-content-end ps-0 mb-0">
+                <div class="col-xl-5 col-lg-5">
+                    <ul class="list-none d-none d-lg-flex justify-content-end ps-0 mb-0">
                         <?php if (!empty($top_header_link_group)): ?>
                             <?php foreach ($top_header_link_group as $single_top_links):
                                 $single_top_link = $single_top_links['link'];
-                                ?>
+                            ?>
                                 <?php if (!empty($single_top_link['url'])):
                                     $target_2 = ($single_top_link['target'] == '_blank') ? "_blank" : ""; ?>
                                     <li class="me-4">
@@ -72,7 +70,7 @@ $schedule = get_field('schedule', 'option');
             </div>
         </div>
     </div>
-    <div class="header-main bg-1B2995">
+    <div class="header-main tpt-20 tpb-20 bg-1B2995">
         <div class="position-relative">
             <div class="container">
                 <div class="row justify-content-between">
@@ -83,59 +81,70 @@ $schedule = get_field('schedule', 'option');
                             </a>
                         <?php endif; ?>
                     </div>
-                    <div class="col-10 d-flex justify-content-end align-items-center">
-                        <div class="d-flex justify-content-center align-items-center">
-                            <nav class="navigation d-flex justify-content-between align-items-center">
-                                <ul class="ps-0 mb-0 list-none d-flex">
-                                    <?php foreach ($menu_link_group as $menu_link_group_single):
-                                        $menu_link_group_menu_selection = $menu_link_group_single['menu_selection'];
-                                        $menu_link_group_link = $menu_link_group_single['link'];
-                                        $menu_link_group_mega_menu = $menu_link_group_single['mega_menu'];
-
-                                        ?>
-
-                                        <?php if ($menu_link_group_menu_selection == 'menu'): ?>
-                                            <div class="position-relative cursor-pointer dpt-30 dpb-30 pe-4">
-                                                <li class="mega-link">
-                                                    <a class="menu-link sans-medium px-3 py-2 font16 radius5 space-0_16 leading20 text-white text-capitalize text-decoration-none transition"
-                                                        href="<?php echo $menu_link_group_link['url']; ?>"><?php echo $menu_link_group_link['title']; ?>
-                                                    </a>
-                                                    <div
-                                                        class="mega-menu opacity-0 position-absolute radius10 px-4 dpt-20 dpb-10 bg-white start-0 transition">
-                                                        <ul class="list-none ps-0 mb-0 w-100">
-                                                            <?php foreach ($menu_link_group_mega_menu as $menu_link_group_mega):
-                                                                $mega_menu_link = $menu_link_group_mega['mega_menu_link'];
-                                                                ?>
-                                                                <li class="dmb-20">
-                                                                    <a class="garamond font20 leading24 text-1B2995 d-inline-flex align-items-center text-capitalize text-decoration-none transition"
-                                                                        href="    <?php echo $mega_menu_link['url']; ?>">
-                                                                        <?php echo $mega_menu_link['title']; ?>
-                                                                    </a>
-                                                                </li>
-                                                            <?php endforeach; ?>
-                                                        </ul>
-                                                    </div>
-                                                </li>
-                                            </div>
-                                        <?php endif; ?>
-
-                                        <?php if ($menu_link_group_menu_selection == 'link'): ?>
-                                            <div class="position-relative cursor-pointer dpt-30 dpb-30 pe-4">
-                                                <li class="mega-link">
-                                                    <a class="menu-link sans-medium px-3 py-2 font16 radius5 space-0_16 leading20 text-white text-capitalize text-decoration-none transition"
-                                                        href="<?php echo $menu_link_group_link['url']; ?>"><?php echo $menu_link_group_link['title']; ?>
-                                                    </a>
-                                                </li>
-                                            </div>
-                                        <?php endif; ?>
-                                    <?php endforeach; ?>
-
-
-                                </ul>
-                            </nav>
+                    <div class="menu-toggle d-lg-none d-flex justify-content-end align-items-center col-6">
+                        <div class="menu-bar-bg radius8 d-flex justify-content-center align-items-center">
+                            <div class="menu-bar d-flex justify-content-center align-items-center">
+                                <a class="d-flex justify-content-center align-items-center" href="">
+                                    <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/templates/icon/white-menu.svg" alt="">
+                                </a>
+                            </div>
+                            <div class="close-bar d-none">
+                                <a href="">
+                                    <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/templates/icon/white-closebar.svg" alt="">
+                                </a>
+                            </div>
                         </div>
+                    </div>
+                    <div class="col-xl-10 col-lg-11 navigation d-none d-lg-flex justify-content-end align-items-center tpt-60 tpb-60">
+                        <nav class="d-flex justify-content-between align-items-center">
+                            <ul class="ps-0 mb-0 list-none d-lg-flex d-block">
+                                <?php foreach ($menu_link_group as $menu_link_group_single):
+                                    $menu_link_group_menu_selection = $menu_link_group_single['menu_selection'];
+                                    $menu_link_group_link = $menu_link_group_single['link'];
+                                    $menu_link_group_mega_menu = $menu_link_group_single['mega_menu'];
 
-                        <div class="ms-5 d-flex justify-content-end align-items-center">
+                                ?>
+
+                                    <?php if ($menu_link_group_menu_selection == 'menu'): ?>
+                                        <div class="position-relative cursor-pointer dpt-30 dpb-30 tpt-0 tpb-0 pe-lg-4 tmb-30">
+                                            <li class="mega-link">
+                                                <a class="menu-link sans-medium px-lg-3 py-lg-2 font16 radius5 space-0_16 leading20 res-font24 text-white text-capitalize text-decoration-none transition"
+                                                    href="<?php echo $menu_link_group_link['url']; ?>"><?php echo $menu_link_group_link['title']; ?>
+                                                </a>
+                                                <div
+                                                    class="mega-menu position-absolute radius10 px-4 dpt-20 dpb-10 bg-white start-0 transition">
+                                                    <ul class="list-none ps-0 mb-0 w-100">
+                                                        <?php foreach ($menu_link_group_mega_menu as $menu_link_group_mega):
+                                                            $mega_menu_link = $menu_link_group_mega['mega_menu_link'];
+                                                        ?>
+                                                            <li class="dmb-20">
+                                                                <a class="garamond font20 leading24 text-1B2995 d-inline-flex align-items-center text-capitalize text-decoration-none transition"
+                                                                    href="    <?php echo $mega_menu_link['url']; ?>">
+                                                                    <?php echo $mega_menu_link['title']; ?>
+                                                                </a>
+                                                            </li>
+                                                        <?php endforeach; ?>
+                                                    </ul>
+                                                </div>
+                                            </li>
+                                        </div>
+                                    <?php endif; ?>
+
+                                    <?php if ($menu_link_group_menu_selection == 'link'): ?>
+                                        <div class="position-relative cursor-pointer dpt-30 dpb-30 tpt-0 tpb-0 pe-lg-4 tmb-30">
+                                            <li class="mega-link">
+                                                <a class="menu-link sans-medium px-lg-3 py-lg-2 font16 radius5 space-0_16 leading20 res-font24 text-white text-capitalize text-decoration-none transition"
+                                                    href="<?php echo $menu_link_group_link['url']; ?>"><?php echo $menu_link_group_link['title']; ?>
+                                                </a>
+                                            </li>
+                                        </div>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+
+
+                            </ul>
+                        </nav>
+                        <div class="ms-lg-5 d-flex justify-content-lg-end justify-content-start align-items-center">
                             <a class="btnA white-border-btn sans-medium font16 space-0_48 leading26 rounded-pill text-decoration-none d-inline-flex justify-content-center align-items-center transition"
                                 href="<<?php echo $header_button['url']; ?>">
                                 <?php echo $header_button['title']; ?>
